89cde4015a4e268d09f1fadea5e3566ae68914f5
Shin <shin.oct12@gmail.com>
2016-03-01 12:07:21 +0900

    テキストを読み込んで連続でコマンドを実行するShellスクリプトのひな型を作成

---
+++ shell/CmdExecutor.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [ $# -ne 1 ]; then
+    echo "Usage: CmdExecutor.sh read_filename"
+    exit 1
+fi
+
+if [ ! -e "$1" ]; then
+    echo "Input file not found. $1"
+    exit 1
+fi
+
+IFS=$'\n'
+file=(`cat "$1"`)
+
+ln=0
+for line in "${file[@]}"; do
+    if [ ! "$(echo $line | cut -c 1)" = "#" ]; then
+        echo $line
+    fi
+done
